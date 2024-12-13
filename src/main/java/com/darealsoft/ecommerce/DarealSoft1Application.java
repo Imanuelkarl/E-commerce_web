3efea678175a533d5d4efbbc5d53ed7d555dbc85
Imanuelkarl <appdeveloperemmanuel@gmail.com>
2024-12-13 10:31:31 +0100

    First Commit

---
+++ src/main/java/com/darealsoft/ecommerce/DarealSoft1Application.java
@@ -0,0 +1,13 @@
+package com.darealsoft.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarealSoft1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarealSoft1Application.class, args);
+	}
+
+}
